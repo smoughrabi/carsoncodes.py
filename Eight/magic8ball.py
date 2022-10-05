e084633711c0c610002d7bd2486bc6a7680c4e75
Sarah El Moughrabi <sarahelmoughrabi@gmail.com>
2022-10-05 20:25:23 +0400

    Update magic8ball.py

--- Eight/magic8ball.py
+++ Eight/magic8ball.py
@@ -44,9 +44,17 @@ col1,col2,col3 = st.columns([1,2,1])
 with col2:
     button = st.button("CLICK ME")
     if button:
-        number = random.randint(1,19)
+        number = random.randint(1,2)
         if number == 1:
-            st.image("Eight/responses/as i see it yes.gif")
+            file_ = open("Eight/responses/as i see it yes.gif", "rb")
+            contents = file_.read()
+            data_url = base64.b64encode(contents).decode("utf-8")
+            file_.close()
+            st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+            unsafe_allow_html=True,
+            )
+            #st.image("Eight/responses/as i see it yes.gif")
         elif number == 2:
             st.image("Eight/responses/ask again later.gif")
         elif number == 3:
